789adf7287eddc274f755e24c064bc9b4372e0ff
k-dorj-ji <157406051+k-dor-ji@users.noreply.github.com>
2025-08-19 23:59:09 -0400

    finished hover and button function

--- etch.js
+++ etch.js
@@ -1,8 +1,38 @@
- const container = document.querySelector("#container");
 let gridSize = 256;
-for (let i = 0; i < gridSize; i++){
+
+function hover(){
+    const div = document.querySelectorAll(".grid");
+    div.forEach(div => {
+        div.addEventListener("mouseover", ()=>{
+            div.classList.add("hovered");
+        })
+        div.addEventListener("mouseout", () => {
+            div.classList.remove("hovered");
+        })
+    })
+}
+
+function gridCreate(gridSize){
+    for (let i = 0; i < gridSize; i++){
     const div = document.createElement("div");
     div.setAttribute('class', 'grid');
+    div.style.flexBasis = `${100/Math.sqrt(gridSize)}%`;
     container.appendChild(div);
-    console.log(div);
+    }
+    hover();
 }
+
+const container = document.querySelector("#container");
+const gridBtn = document.querySelector(".grid-size");
+
+gridCreate(gridSize);
+
+gridBtn.addEventListener("click", () => {
+    gridSize = prompt("Enter the grid size: "); // fix to ensure input is always a number and is below 100
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    gridCreate(gridSize);
+    hover();
+
+})
